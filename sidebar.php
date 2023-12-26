15e2843b1f1e2fa5dcf89c829e2a624f82b0a0e8
Renzi Fidele <renzifidele2001@gmail.com>
2023-12-26 11:59:26 +0200

    Melhorias do slider dos banners

--- sidebar.php
+++ sidebar.php
@@ -4,38 +4,20 @@
          <!--Sliders contendo o banner-->
          <ul class="slide-banners">
             <li>
-               <a href="https://yticon.com.br/empreendimentos/londrina/solar-di-milano" title="Solar di Milano">
-                  <img src="wp-content/uploads/2023/06/bannerblog_solardimilano.png" alt="" />
+               <a href="https://construtoradesucesso.com.br/empreendimentos/detalhes/solar-de-hamburgo.html" title="Solar de Hamburgo">
+                  <img src="<?php bloginfo('template_directory');?>/build/img/banners/banner1.jpg" alt="AMC Blog" />
                </a>
             </li>
 
             <li>
-               <a href="https://yticon.com.br/empreendimentos/campinas/graciosa-mansoes" title="Graciosa Mansões">
-                  <img src="wp-content/uploads/2023/03/BANNER-LATERAL-BLOG-2888X419PX.png" alt="" />
+               <a href="https://construtoradesucesso.com.br/empreendimentos/detalhes/ilha-di-capri.html" title="Ilha di Capri">
+                  <img src="<?php bloginfo('template_directory');?>/build/img/banners/banner2.jpg" alt="AMC Blog" />
                </a>
             </li>
 
             <li>
-               <a href="https://yticon.com.br/empreendimentos/londrina/hype" title="Banner Hype">
-                  <img src="wp-content/uploads/2023/02/bannerblog-hype-1.jpg" alt="" />
-               </a>
-            </li>
-
-            <li>
-               <a href="wp-content/uploads/2022/08/ay-brisas-banner-blog.jpg" title="Brisas Bella Citta">
-                  <img src="wp-content/uploads/2022/08/ay-brisas-banner-blog.jpg" alt="" />
-               </a>
-            </li>
-
-            <li>
-               <a href="https://yticon.com.br/empreendimentos/maringa/up-residence" title="Conheça o Up! Yticon">
-                  <img src="wp-content/uploads/2022/04/YT-UP-BANNER_LATERALBLOG_288X419PX_F.jpg" alt="" />
-               </a>
-            </li>
-
-            <li>
-               <a href="https://yticon.com.br/empreendimentos/londrina/vibe" title="Vibe Yticon">
-                  <img src="wp-content/uploads/2022/03/BANNER-LATERAL-BLOG.png" alt="" />
+               <a href="https://construtoradesucesso.com.br/empreendimentos/detalhes/solar-dos-alamos.html" title="Solar dos Álamos">
+                  <img src="<?php bloginfo('template_directory');?>/build/img/banners/banner3.jpg" alt="AMC Blog" />
                </a>
             </li>
          </ul>
